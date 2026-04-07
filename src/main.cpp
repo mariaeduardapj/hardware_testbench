c1d27f5c3dae156d1ce890ee033bacd9552d2445
Maria Eduarda P. Jesus <mariaeduardapereiraje@gmail.com>
2026-04-07 07:42:50 -0300

    feat: add user confirmation system
    Added user confirmation system to validate if hardware tests passed.
    
    New features:
    - Pass/fail validation logic
    
    Next steps:
    - Add relay test
    - Improve code organization

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 #define SYSTEM_NAME "Hardware Test Bench"
-#define SYSTEM_VERSION "v0.1.0"
+#define SYSTEM_VERSION "v0.2.0"
 #define BUZZER_PIN 15
 
 void setup() {
@@ -18,23 +18,43 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available())  {
-
+  if (Serial.available()) {
     String input = Serial.readStringUntil('\n'); // Read the input until a newline character is encountered
     input.trim(); // Remove any leading or trailing whitespace
 
     char Start = input.charAt(0); // Get the first character of the trimmed string
     if (Start == 'y' || Start == 'Y') {
+      Serial.print("\nStarting Buzzer Test... ");
       digitalWrite(BUZZER_PIN, LOW);
-      delay(500);
+      delay(50);
       digitalWrite(BUZZER_PIN, HIGH);
-      Serial.println("Buzzer Test completed. Start another test? (y/n)");
-    }
+      delay(500);
+      digitalWrite(BUZZER_PIN, LOW);
+      delay(50);
+      digitalWrite(BUZZER_PIN, HIGH);
+      delay(1000);
+      Serial.println("Buzzer Test completed.");
+      delay(500);
+      while (Serial.available()) {
+        Serial.read();
+      }
+      delay(500);
+      Serial.println("Did you hear the two short beeps? (y/n)");
+      while (Serial.available() == 0) {
+      }
+      char Response = Serial.read();
+      if (Response == 'y' || Response == 'Y') {
+        Serial.println("Buzzer is working correctly. Start another test? (y/n)");
+      }
+      else {
+        Serial.println("Buzzer test failed or not confirmed. Start another test? (y/n)");
+      }
+    } 
     else if (Start == 'n' || Start == 'N') {
-      Serial.println("Buzzer Test skipped. Start test now? (y/n)");
+      Serial.println("Buzzer Test skipped. Start the test now? (y/n)");
     }
     else {
-      Serial.println("Invalid input. Please enter 'y' or 'n'.");
+      Serial.println("Invalid test input. Please enter 'y' or 'n'.");
     }
   }
   delay(100);
